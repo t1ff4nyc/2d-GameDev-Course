f83e060cf4054316a2686a79db272e82efab14f1
t1ff4nyc <tichen17@s.sfusd.edu>
2024-03-18 14:41:03 -0700

    L3 P1 practice completed

--- 04.to_space_and_beyond_workbook/ship.gd
+++ 04.to_space_and_beyond_workbook/ship.gd
@@ -3,8 +3,11 @@ extends Sprite2D
 var max_speed := 600.0
 var velocity := Vector2(0, 0)
 
-
 func _process(delta: float) -> void:
 	var direction := Vector2(0, 0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
+
+	velocity = direction * max_speed
+	position += velocity * delta
+	rotation = velocity.angle()
