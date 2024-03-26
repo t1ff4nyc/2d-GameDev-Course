e961f45101089ffda8d0a31467fe107803dfc6f8
t1ff4nyc <tichen17@s.sfusd.edu>
2024-03-26 14:04:37 -0700

    L3 P2 practice completed

--- 04.to_space_and_beyond_workbook/practices/L3.P2.character_diagonals/character_diagonals.gd
+++ 04.to_space_and_beyond_workbook/practices/L3.P2.character_diagonals/character_diagonals.gd
@@ -2,8 +2,6 @@ extends Sprite2D
 
 var max_speed := 600.0
 var velocity := Vector2(0, 0)
-# Once again, the direction variable is outside the _process() function so the
-# practice testing code can read its value.
 var direction := Vector2(0, 0)
 
 
@@ -11,8 +9,8 @@ func _process(delta: float) -> void:
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
 
-	# The character is way too fast, but only when moving diagonally!
-	# Add code to prevent that.
+	if direction.length() > 1.0: 
+		direction = direction.normalized() 
 
 	velocity = direction * max_speed
 	position += velocity * delta
