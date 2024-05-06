167981bce95d7de5fccfc9f6429211abf0e15f67
t1ff4nyc <tichen17@s.sfusd.edu>
2024-05-06 14:32:43 -0700

    L3 P1 complete

--- M06.looting_workbook/lessons/chest/CanvasGroup.gd
+++ M06.looting_workbook/lessons/chest/CanvasGroup.gd
@@ -5,8 +5,7 @@ extends Area2D
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
-	canvas_group.material.set_shader_parameter("line_thickness", 10.0)
-
+	canvas_group.material.set_shader_parameter("line_thickness", 3.0)
 
 func set_outline_thickness(new_thickness: float) -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
@@ -18,4 +17,3 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
 	var tween := create_tween()
 	tween.tween_method(set_outline_thickness, 6.0, 3.0, 0.08)
-	canvas_group.material.set_shader_parameter("line_thickness", 5.0)	
