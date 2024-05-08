9ccb20e28d0a91a25ecbdc7de7a91af84f570553
t1ff4nyc <tichen17@s.sfusd.edu>
2024-05-08 10:25:34 -0700

    L5 P1 complete

--- M06.looting_workbook/lessons/chest/chest.gd
+++ M06.looting_workbook/lessons/chest/chest.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+
 @onready var canvas_group: CanvasGroup = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
@@ -13,8 +15,22 @@ func set_outline_thickness(new_thickness: float) -> void:
 func _on_mouse_entered() -> void:
 	var tween := create_tween()
 	tween.tween_method(set_outline_thickness, 3.0, 6.0, 0.08)
-	queue_free()
 
 func _on_mouse_exited() -> void:
 	var tween := create_tween()
 	tween.tween_method(set_outline_thickness, 6.0, 3.0, 0.08)
+
+func _input_event(viewport: Node, event: InputEvent, shape_index: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+
+	if event_is_mouse_click:
+		open()
+
+func open() -> void:
+	animation_player.play("open")
+
+	input_pickable = false
--- M06.looting_workbook/practices/L5.P1.open_the_door/door.gd
+++ M06.looting_workbook/practices/L5.P1.open_the_door/door.gd
@@ -1,13 +1,36 @@
 extends Area2D
 
-@onready var animation_player: AnimationPlayer = $"AnimationPlayer"
 
+@onready var canvas_group: CanvasGroup = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+	canvas_group.material.set_shader_parameter("line_thickness", 3.0)
+
+func set_outline_thickness(new_thickness: float) -> void:
+	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
+
+func _on_mouse_entered() -> void:
+	var tween := create_tween()
+	tween.tween_method(set_outline_thickness, 3.0, 6.0, 0.08)
+
+func _on_mouse_exited() -> void:
+	var tween := create_tween()
+	tween.tween_method(set_outline_thickness, 6.0, 3.0, 0.08)
 
 func _input_event(viewport: Node, event: InputEvent, shape_index: int):
-	# Complete this boolean expression to check if the input event is a left-mouse click.
 	var event_is_mouse_click: bool = (
-		false
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
 	)
 
 	if event_is_mouse_click:
-		pass
+		open()
+
+func open() -> void:
+	animation_player.play("open")
+
+	input_pickable = false
